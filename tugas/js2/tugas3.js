fbbd1b5dae90113bb8021d0732d9aee02459009f
ridwan bahtiar <ridwanbahtiar15@gmail.com>
2023-09-07 19:01:04 +0700

    initial commit

--- tugas/js2/tugas3.js
+++ tugas/js2/tugas3.js
@@ -31,5 +31,3 @@ function cekNIlai(nilaiAwal, nilaiAKhir, dataArray) {
 
 console.log(cekNIlai(5, 20, [2, 25, 4, 14, 17, 30, 10]));
 console.log(cekNIlai(15, 3, [2, 25, 4, 14, 17, 30, 8]));
-console.log(cekNIlai(4, 17, [2, 25, 4]));
-console.log(cekNIlai(5, 17, [2, 25, 4, 1, 30, 18]));
